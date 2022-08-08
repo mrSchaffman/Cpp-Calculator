cf1f1e0ecf34411d4135910710f8f1f762b3d671
Mr.Schaffman <61944369+mrSchaffman@users.noreply.github.com>
2022-08-08 17:05:32 +0200

    Controller: Manager class declaration

--- Calculator/controller/Command.h
+++ Calculator/controller/Command.h
@@ -25,6 +25,9 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 #include<memory>
+using std::unique_ptr;
+using std::shared_ptr;
+
 namespace controller
 {
 	// Command Pattern
@@ -70,7 +73,7 @@ namespace controller
 
 	// helper
 	inline void commandDeleter(Command*p) { p->release(); }
-	using CommandPtr = std::unique_ptr<Command, decltype(&commandDeleter)>;
+	using CommandPtr = unique_ptr<Command, decltype(&commandDeleter)>;
 	inline auto make_command_ptr(Command* p) { return CommandPtr{ p, &commandDeleter }; }
 	
 	template<typename T, typename...Args>
