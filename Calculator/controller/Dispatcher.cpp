5b452cb2a27d9a7d7820aaf1c1faede072489c9d
Mr.Schaffman <61944369+mrSchaffman@users.noreply.github.com>
2022-08-08 16:58:10 +0200

    controller: Dispatcher update

--- Calculator/controller/Dispatcher.cpp
+++ Calculator/controller/Dispatcher.cpp
@@ -23,14 +23,19 @@
 */
 
 #include"Command.h"
+#include"CommandRegistry.h"
+#include"Exception.h"
+#include<regex>
+#include<sstream>
 namespace controller
 {
 	class Dispatcher::DispatcherImpl
 	{
 	public:
 		explicit DispatcherImpl(view::UserInterface&ui) :m_ui{ ui } {}
-		void onCommandEntered(const std::string& command);
-
+		void execute(const std::string& command);
+		void printHelp()const;
+		bool isDigit(const std::string&cmd, double&d);
 	private:
 		view::UserInterface&m_ui;
 	};
@@ -44,13 +49,70 @@ namespace controller
 	{
 	}
 
-	void Dispatcher::onCommandEntered(const std::string & command)
+	void Dispatcher::onCommandEntered(const std::string & cmd)
 	{
-		pImpl->onCommandEntered(command);
+		pImpl->execute(cmd);
 	}
 
-	void Dispatcher::DispatcherImpl::onCommandEntered(const std::string & command)
+	void Dispatcher::DispatcherImpl::execute(const std::string & cmd)
 	{
+		double d;
+		if (cmd == "help")
+			printHelp();
+		//else if (isDigit(cmd,d))
+		//	// todo
+		//else if (cmd == "undo")
+		//	// todo
+		//else if (cmd == "redo")
+		//	// todo
+		else
+		{
+			auto c = CommandRegistry::getInstance().getCommandByName(cmd);
+			if (!c)
+			{
+				std::ostringstream oss;
+				oss << "Command '" << cmd << "' is undefined";
+				m_ui.updateDisplay(oss.str());
+			}
+
+			try
+			{
+				// pass the Command to the Manager...
+				// to do
+			}
+			catch (utility::Exception&e)
+			{
+				m_ui.updateDisplay(e.what());
+			}
+		}
+	}
+
+	void Dispatcher::DispatcherImpl::printHelp() const
+	{
+		std::ostringstream oss;
+		oss << "-----------------------------------\n"
+			<< "----undo: undo last operation.\n---"
+			<< "----redo: redo last operation.\n---";
+		set<string> cmdNames = CommandRegistry::getInstance().getAllCommandNames;
+		for (auto& name : cmdNames)
+			oss << "----" << CommandRegistry::getInstance().getCommandByName(name)->getHelp() << "---\n";
+
+		m_ui.updateDisplay(oss.str());
+	}
+
+	bool Dispatcher::DispatcherImpl::isDigit(const std::string & cmd, double & d)
+	{
+		if (cmd == "+" || cmd == "-") return false;
+
+		std::regex dpRegex("((\\+|-)?[[:digit:]]*)(\\.(([[:digit:]]+)?))?((e|E)((\\+|-)?)[[:digit:]]+)?");
+		bool isNumber{ std::regex_match(cmd, dpRegex) };
+
+		if (isNumber)
+		{
+			d = std::stod(cmd);
+		}
+
+		return isNumber;
 	}
 
 }
